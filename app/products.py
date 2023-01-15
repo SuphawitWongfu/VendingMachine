f43ea5fd18e513864d47aa636c63ebb7228aab13
suphawit.won@student.mahidol.edu <you@example.com>
2023-01-15 11:51:53 +0700

    Added edit and add apis for products.py

--- app/products.py
+++ app/products.py
@@ -8,7 +8,7 @@ This file contains CRUD operation regarding products table
 products = Blueprint('products', __name__)
 
 
-@products.route("/add_products/")
+@products.route("/add_products/", methods=["GET", "POST"])
 def add_products():
     args = request.args
     if args and all(k in args for k in ("name", "code", "quantity", "price")):
@@ -21,11 +21,11 @@ def add_products():
         except:
             session.close()
         session.close()
-    return view_vending_machine()
+    return redirect(url_for("products.view_products"))
 
 
 @products.route("/products/", methods=["GET"])
-def view_vending_machine():
+def view_products():
     session = Session()
     queries = session.query(Products).all()
     session.close()
@@ -37,3 +37,40 @@ def view_vending_machine():
                    'price': query.price_per_unit, 'quantity': query.product_quantity}
         prods.append(product)
     return jsonify(prods)
+
+
+@products.route("/edit_products/", methods=["GET", "POST"])
+def edit_vending_machine():
+    args = request.args
+    if args and "id" in args:
+        session = Session()
+        current_product = session.query(Products).filter_by(id=args["id"]).first()
+        if "name" in args:
+            current_product.product_name = args["name"]
+        if "code" in args:
+            current_product.product_code = args["code"]
+        if "quantity" in args:
+            current_product.product_quantity = args["quantity"]
+        if "price" in args:
+            current_product.price_per_unit = args["price"]
+        try:
+            session.commit()
+        except:
+            session.close()
+        session.close()
+    return redirect(url_for("products.view_products"))
+
+
+@products.route('/delete_products/', methods=["GET", "POST", "DELETE"])
+def delete_vending_machine():
+    args = request.args
+    if args and "id" in args:
+        session = Session()
+        unwanted_product = session.query(Products).filter_by(id=args["id"]).first()
+        try:
+            session.delete(unwanted_product)
+            session.commit()
+        except:
+            session.close()
+        session.close()
+    return redirect(url_for("products.view_products"))
