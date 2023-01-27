a8b1788555a91fad6003b8ec02b97d979197c2ce
Suphawit <suphawit.won@gmail.com>
2023-01-27 09:25:23 +0700

    Added unit tests

---
+++ app/tests/test_add_object_to_db.py
@@ -0,0 +1,69 @@
+from app.queryUtils import *
+
+def test_add_vending_machine() :
+    session = Session()
+    new_vending_machine = Vending_machine("test", "test")
+    before_add_vending_machine_list = session.query(Vending_machine).all()
+    session.close()
+    addObjToDB(new_vending_machine)
+    session = Session()
+    after_add_vending_machine_list = session.query(Vending_machine).all()
+    session.query(Vending_machine).delete()
+    session.commit()
+    session.close()
+    assert len(after_add_vending_machine_list) - len(before_add_vending_machine_list) == 1
+
+def test_add_products() :
+    session = Session()
+    new_product = Products("test_product_name", 1, 10, 10)
+    before_add_product_list = session.query(Products).all()
+    session.close()
+    addObjToDB(new_product)
+    session = Session()
+    after_add_product_list = session.query(Products).all()
+    session.query(Products).delete()
+    session.commit()
+    session.close()
+    assert len(after_add_product_list) - len(before_add_product_list) == 1
+
+def test_add_duplicate_products() :
+    new_product = Products("test_product_name", 1, 10, 10)
+    addObjToDB(new_product)
+    session = Session()
+    before_add_duplicate_product_list = session.query(Products).all()
+    session.close()
+    addObjToDB(new_product)
+    session = Session()
+    after_add_duplicate_product_list = session.query(Products).all()
+    session.query(Products).delete()
+    session.commit()
+    session.close()
+    assert len(after_add_duplicate_product_list) == len(before_add_duplicate_product_list)
+
+def test_add_stock() :
+    new_product = Products("test_product_name", 1, 10, 10)
+    new_vending_machine = Vending_machine("test", "test")
+    addObjToDB(new_product)
+    addObjToDB(new_vending_machine)
+    session = Session()
+    lastest_vending_machine = session.query(Vending_machine).first()
+    lastest_product = session.query(Products).first()
+    session.close()
+    new_stock = MachineStock(lastest_vending_machine.id, lastest_product.id, 5)
+    session = Session()
+    before_add_stock_list = session.query(MachineStock).all()
+    session.close()
+    addObjToDB(new_stock)
+    session = Session()
+    after_add_stock_list = session.query(MachineStock).all()
+    session.query(MachineStock).delete()
+    session.query(Vending_machine).delete()
+    session.query(Products).delete()
+    session.commit()
+    session.close()
+    assert len(after_add_stock_list) - len(before_add_stock_list) == 1
+
+
+
+
+
---
+++ app/tests/test_edit_object_in_db.py
@@ -0,0 +1,76 @@
+from app.queryUtils import updateDatabaseRowByID
+from app.engine import Session
+from app.schema import *
+
+def setup_object_for_edit(table_name, row_object) :
+    session = Session()
+    session.add(row_object)
+    session.commit()
+    to_be_edited_object = session.query(table_name).first()
+    session.close()
+    return to_be_edited_object
+
+def clear_db_tables() :
+    session = Session()
+    session.query(MachineStock).delete()
+    session.query(Vending_machine).delete()
+    session.query(Products).delete()
+    session.commit()
+    session.close()
+
+def test_edit_vending_machine() :
+    new_vending_machine = Vending_machine("before edit name", "before edit column")
+    to_be_edited_vending_machine = setup_object_for_edit(Vending_machine, new_vending_machine)
+    mock_query_strings = {"id": to_be_edited_vending_machine.id,
+                          "machine_name": "after_edit_name",
+                          "machine_location": "after_edit_location"}
+    updateDatabaseRowByID(Vending_machine,mock_query_strings)
+    session = Session()
+    after_edit_vending_machine = session.query(Vending_machine).filter_by(id=mock_query_strings["id"]).first()
+    session.close()
+    name_is_edited = after_edit_vending_machine.machine_name == mock_query_strings["machine_name"]
+    location_is_edited = after_edit_vending_machine.machine_location == mock_query_strings["machine_location"]
+    clear_db_tables()
+    assert name_is_edited
+    assert location_is_edited
+
+def test_edit_product() :
+    new_product = Products("before_edit_name", 1, 0, 0)
+    to_be_edited_product = setup_object_for_edit(Products, new_product)
+    mock_query_strings = {"id": to_be_edited_product.id,
+                          "product_name": "after_edit_name",
+                          "product_code": 11,
+                          "product_quantity": 6,
+                          "price_per_unit": 9}
+    updateDatabaseRowByID(Products,mock_query_strings)
+    session = Session()
+    after_edit_product = session.query(Products).filter_by(id=mock_query_strings["id"]).first()
+    session.close()
+    name_is_edited = after_edit_product.product_name == mock_query_strings["product_name"]
+    code_is_edited = after_edit_product.product_code == mock_query_strings["product_code"]
+    quantity_is_edited = after_edit_product.product_quantity == mock_query_strings["product_quantity"]
+    price_is_edited = after_edit_product.price_per_unit == mock_query_strings["price_per_unit"]
+    clear_db_tables()
+    assert name_is_edited
+    assert code_is_edited
+    assert quantity_is_edited
+    assert price_is_edited
+
+def test_edit_stock() :
+    clear_db_tables()
+    new_vending_machine = Vending_machine("before edit name", "before edit column")
+    new_product = Products("before_edit_name", 1, 1, 1)
+    vending_machine_for_stock = setup_object_for_edit(Vending_machine, new_vending_machine)
+    product_for_stock = setup_object_for_edit(Products, new_product)
+
+    new_stock = MachineStock(vending_machine_for_stock.id, product_for_stock.id, 1)
+    stock_for_edit = setup_object_for_edit(MachineStock, new_stock)
+    mock_query_strings = {"id": stock_for_edit.id,
+                          "quantity": 0}
+    updateDatabaseRowByID(MachineStock, mock_query_strings)
+    session = Session()
+    after_edit_stock = session.query(MachineStock).filter_by(id=mock_query_strings["id"]).first()
+    session.close()
+    quantity_is_edited = after_edit_stock.quantity == mock_query_strings["quantity"]
+    clear_db_tables()
+    assert quantity_is_edited
