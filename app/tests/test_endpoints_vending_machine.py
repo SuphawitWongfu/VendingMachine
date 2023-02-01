d79d3208e3c0fe495f3a31ab7f3070798a6ed28a
Suphawit <suphawit.won@gmail.com>
2023-02-01 23:19:34 +0700

    Refactor vending machine endpoit unittest

--- app/tests/test_endpoints_vending_machine.py
+++ app/tests/test_endpoints_vending_machine.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import pytest
 from flask.testing import FlaskClient
 from werkzeug.test import TestResponse
@@ -11,6 +13,21 @@ class MachineTester(Tester):
     def get_all_machines(self) -> TestResponse:
         return self.client.get("/vendings/")
 
+    def add_machines(self, test_data: Dict[str, str | int]) -> TestResponse:
+        return self.client.post(
+            "/add_vendings/", query_string=test_data, follow_redirects=True
+        )
+
+    def delete_machines(self, test_data: Dict[str, str | int]) -> TestResponse:
+        return self.client.delete(
+            "/delete_vendings/", query_string=test_data, follow_redirects=True
+        )
+
+    def edit_machines(self, test_data: Dict[str, str | int]) -> TestResponse:
+        return self.client.post(
+            "/edit_vendings/", query_string=test_data, follow_redirects=True
+        )
+
 
 @pytest.fixture
 def tester(client: FlaskClient) -> MachineTester:
@@ -23,18 +40,16 @@ def test_view_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
 
 
-def test_adding_vending_machine(client: FlaskClient) -> None:
+def test_adding_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
     test_data = {"machine_name": "new_edit_name", "machine_location": "new_location"}
-    response = client.post(
-        "/add_vendings/", query_string=test_data, follow_redirects=True
-    )
+    response = tester.add_machines(test_data)
     assert response.status_code == 200 or response.status_code == 204
     assert len(response.json) != 0
     clear_db(vendingMachine)
 
 
-def test_edit_vending_machine(client: FlaskClient) -> None:
+def test_edit_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
     add_obj_to_db(vendingMachine("before_edit", "before_edit"))
     test_entry = get_test_entry(vendingMachine)
@@ -43,9 +58,7 @@ def test_edit_vending_machine(client: FlaskClient) -> None:
         "machine_name": "after_edit_name",
         "machine_location": "after_edit_location",
     }
-    response = client.post(
-        "/edit_vendings/", query_string=test_data, follow_redirects=True
-    )
+    response = tester.edit_machines(test_data)
     edited_test_entry = get_test_entry(vendingMachine)
     assert response.status_code == 200 or response.status_code == 204
     assert len(response.json) != 0
@@ -55,29 +68,25 @@ def test_edit_vending_machine(client: FlaskClient) -> None:
     clear_db(vendingMachine)
 
 
-def test_delete_vending_machine(client: FlaskClient) -> None:
+def test_delete_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
     add_obj_to_db(vendingMachine("test_name", "test_location"))
     test_entry = get_test_entry(vendingMachine)
-    response = client.delete(
-        "/delete_vendings/", query_string={"id": test_entry.id}, follow_redirects=True
-    )
+    response = tester.delete_machines({"id": test_entry.id})
     assert response.status_code == 200 or response.status_code == 204
     assert response.json is None
     clear_db(vendingMachine)
 
 
-def test_failed_add_vending_machine(client: FlaskClient) -> None:
+def test_failed_add_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
-    response = client.get("/add_vendings/", query_string={}, follow_redirects=True)
+    response = tester.add_machines({})
     assert response.status_code == 400
     clear_db(vendingMachine)
 
 
-def test_failed_delete_vending_machine(client: FlaskClient) -> None:
+def test_failed_delete_vending_machine(tester: MachineTester) -> None:
     clear_db(vendingMachine)
-    response = client.delete(
-        "/delete_vendings/", query_string={}, follow_redirects=True
-    )
+    response = tester.delete_machines({})
     assert response.status_code == 400
     clear_db(vendingMachine)
