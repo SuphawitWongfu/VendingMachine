6677640f1a91cd7243e85aa002dd5cc66bb54b08
Suphawit <suphawit.won@gmail.com>
2023-02-01 21:40:06 +0700

    Added product endpoint unittest

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,8 +1,14 @@
+from dataclasses import dataclass
+from typing import Type
+
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
+from werkzeug.test import TestResponse
 
 from app.app import create_app
+from app.database.engine import Session
+from app.database.schema import vendingMachine, MachineStock, Products
 
 
 @pytest.fixture()
@@ -24,3 +30,29 @@ def app():
 @pytest.fixture()
 def client(app: Flask) -> FlaskClient:
     return app.test_client()
+
+
+def clear_db(table_class: Type[vendingMachine | Products | MachineStock]) -> None:
+    session = Session()
+    session.query(table_class).delete()
+    session.commit()
+    session.close()
+
+
+def get_test_entry(
+    table_class: Type[vendingMachine | Products | MachineStock],
+) -> vendingMachine | Products | MachineStock:
+    session = Session()
+    test_entry = session.query(table_class).first()
+    session.close()
+    return test_entry
+
+
+@dataclass
+class Tester:
+    __test__ = False
+    client: FlaskClient
+
+    @staticmethod
+    def expect(response: TestResponse, code: int) -> bool:
+        return response.status_code == code
--- app/tests/test_endpoints_vending_machine.py
+++ app/tests/test_endpoints_vending_machine.py
@@ -1,33 +1,29 @@
-from typing import Type
-
+import pytest
 from flask.testing import FlaskClient
+from werkzeug.test import TestResponse
 
-from app.database.engine import Session
 from app.database.queryUtils import add_obj_to_db
 from app.database.schema import vendingMachine
+from app.tests.conftest import clear_db, get_test_entry, Tester
 
 
-def clear_db() -> None:
-    session = Session()
-    session.query(vendingMachine).delete()
-    session.commit()
-    session.close()
+class MachineTester(Tester):
+    def get_all_machines(self) -> TestResponse:
+        return self.client.get("/vendings/")
 
 
-def get_test_entry() -> Type[vendingMachine]:
-    session = Session()
-    test_entry = session.query(vendingMachine).first()
-    session.close()
-    return test_entry
+@pytest.fixture
+def tester(client: FlaskClient) -> MachineTester:
+    return MachineTester(client=client)
 
 
-def test_view_vending_machine(client: FlaskClient):
-    response = client.get("/vendings/")
-    assert response.status_code == 200 or response.status_code == 204
+def test_view_vending_machine(tester: MachineTester) -> None:
+    response = tester.get_all_machines()
+    assert Tester.expect(response, 200) or Tester.expect(response, 204)
 
 
-def test_adding_vending_machine(client: FlaskClient):
-    clear_db()
+def test_adding_vending_machine(client: FlaskClient) -> None:
+    clear_db(vendingMachine)
     test_data = {"machine_name": "new_edit_name", "machine_location": "new_location"}
     response = client.post(
         "/add_vendings/", query_string=test_data, follow_redirects=True
@@ -36,10 +32,10 @@ def test_adding_vending_machine(client: FlaskClient):
     assert len(response.json) != 0
 
 
-def test_edit_vending_machine(client: FlaskClient):
-    clear_db()
+def test_edit_vending_machine(client: FlaskClient) -> None:
+    clear_db(vendingMachine)
     add_obj_to_db(vendingMachine("before_edit", "before_edit"))
-    test_entry = get_test_entry()
+    test_entry = get_test_entry(vendingMachine)
     test_data = {
         "id": test_entry.id,
         "machine_name": "after_edit_name",
@@ -48,7 +44,7 @@ def test_edit_vending_machine(client: FlaskClient):
     response = client.post(
         "/edit_vendings/", query_string=test_data, follow_redirects=True
     )
-    edited_test_entry = get_test_entry()
+    edited_test_entry = get_test_entry(vendingMachine)
     assert response.status_code == 200 or response.status_code == 204
     assert len(response.json) != 0
     assert edited_test_entry.id == test_data["id"]
@@ -56,10 +52,10 @@ def test_edit_vending_machine(client: FlaskClient):
     assert edited_test_entry.machine_location == test_data["machine_location"]
 
 
-def test_delete_vending_machine(client: FlaskClient):
-    clear_db()
+def test_delete_vending_machine(client: FlaskClient) -> None:
+    clear_db(vendingMachine)
     add_obj_to_db(vendingMachine("test_name", "test_location"))
-    test_entry = get_test_entry()
+    test_entry = get_test_entry(vendingMachine)
     response = client.delete(
         "/delete_vendings/", query_string={"id": test_entry.id}, follow_redirects=True
     )
@@ -67,14 +63,14 @@ def test_delete_vending_machine(client: FlaskClient):
     assert response.json is None
 
 
-def test_failed_add_vending_machine(client: FlaskClient):
-    clear_db()
+def test_failed_add_vending_machine(client: FlaskClient) -> None:
+    clear_db(vendingMachine)
     response = client.get("/add_vendings/", query_string={}, follow_redirects=True)
     assert response.status_code == 400
 
 
-def test_failed_delete_vending_machine(client: FlaskClient):
-    clear_db()
+def test_failed_delete_vending_machine(client: FlaskClient) -> None:
+    clear_db(vendingMachine)
     response = client.delete(
         "/delete_vendings/", query_string={}, follow_redirects=True
     )
